e867ac285e2bd03d93f39638054614b6ad5af417
Cherie Young <44423254+youncher@users.noreply.github.com>
2020-02-06 20:27:04 -0800

    Added run method to Manager.

--- src/Manager.java
+++ src/Manager.java
@@ -1,13 +1,53 @@
+import java.util.Scanner;
+
 class Manager {
+
     //private List<Item> todoList;
     private Menu menu;
+    private String prompt;
+    private Scanner scanner;
 
     public Manager() {
         menu = new Menu();
+        this.prompt = "Select an option: ";
+        scanner = new Scanner(System.in);
     }
 
     public void run() {
-        menu.showMainMenu();
+        while (true) {
+            menu.showMainMenu();
+            int input = getInput();
+
+            switch (input) {
+                case 1:
+                    System.out.println("Add todo item");
+                    //addNewItem();
+                    break;
+                case 2:
+                    System.out.println("View todo list");
+                    break;
+                case 3:
+                    System.out.println("Delete todo item");
+                    break;
+                case 4:
+                    System.out.println("Edit todo item");
+                    break;
+                case 5:
+                    System.out.println("Export list to file");
+                    break;
+                case 6:
+                    System.out.println("Quit");
+                    System.exit(0);
+                    break;
+            }
+            System.out.println();
+        }
+    }
+
+    private int getInput() {
+        System.out.print(prompt);
+        int input = scanner.nextInt();
+        return input;
     }
 
     /*public List<Item> getTodoList() {
--- src/Menu.java
+++ src/Menu.java
@@ -10,15 +10,18 @@ public class Menu {
         this.appTitle = "To-Do List";
     }
 
-
     public void showMainMenu() {
         System.out.println(separator);
         System.out.println(appTitle);
         System.out.println();
         System.out.println("Main Menu");
         System.out.println();
-        System.out.println("\t1. Add new todo item"); // 2. Aisjflsjdf. 3. Sofoisjfdiosf Choose an option: );
-        //System.out.println();
+        System.out.println("\t1. Add new todo item");
+        System.out.println("\t2. View todo list");
+        System.out.println("\t3. Delete todo item");
+        System.out.println("\t4. Edit todo item");
+        System.out.println("\t5. Export list to file");
+        System.out.println("\t6. Quit");
         System.out.println(separator);
     }
 
