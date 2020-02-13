8b9341474088f4ac7ca60b51d04a0105553221d7
ritter327 <ritterma@oregonstate.edu>
2020-02-12 17:39:29 -0700

    Matt and Jonathan implemented add item functionality.

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,57 @@
+import java.util.ArrayList;
+
 public class Item {
     private String title;
 
+    //Matt Edits 2.11.20
+    private String location;
+    private int numPeople;
+    private ArrayList<String> peopleList;
+
+    private String dueDate;
+    private String priority;
+
     public Item(String title) {
         this.title = title;
+        peopleList = new ArrayList<String>();
     }
 
-    public void printItem(){
-        System.out.println(title);
+    public void addLocation(String str){
+        location = str;
+    }
+
+    public void setNumPeople(int num){
+        numPeople = num;
+    }
+    public void addPerson(String str){
+        peopleList.add(str);
+    }
+
+    @Override
+    public String toString() {
+        return getTitle() + " " + getLocation() + " " + getPeople() + " " + getDueDate() + " " + getPriority();
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    }
+    public String getPeople(){
+        StringBuffer sb = new StringBuffer();
+        for(String s: peopleList){            //For every string in people list
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDueDate(){
+        return dueDate;
+    }
+
+    public String getPriority(){
+        return priority;
     }
 
 }
